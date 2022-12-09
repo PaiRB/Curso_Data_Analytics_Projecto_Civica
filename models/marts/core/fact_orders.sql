ef199b8d6069b18a59aa845ae80ef3bfe92519ef
pablo-r.b@hotmail.com <pablo-r.b@hotmail.com>
2022-12-09 22:52:50 +0000

    fg

--- models/marts/core/fact_orders.sql
+++ models/marts/core/fact_orders.sql
@@ -22,7 +22,7 @@ WITH fact_orders AS (
 
 SELECT
     -- ids
-    {{ dbt_utils.generate_surrogate_key(['order_id', 'product_id']) }} AS fact_order_id
+    md5(CONCAT(order_id,product_id,fivetran_synced)) AS fact_order_id  
     , order_id
     , natural_order_id
     , product_id
